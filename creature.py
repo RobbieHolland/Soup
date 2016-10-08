92c6a1c52ad08913cdeda0f195a4bb03a3b53046
Robert <Robert@dyn1181-178.wlan.ic.ac.uk>
2016-10-08 18:02:49 +0100

    Made creature draw method specific to creature object

--- creature.py
+++ creature.py
@@ -6,15 +6,13 @@ import random as rd
 import numpy as np
 
 class Creature:
-    creature_count = 0
-
     def __init__(self, x, y, radius, colour, inputs, hidden, output):
         """
         @param colour
         """
 
         # Characteristics
-        self.life = 0
+        self.life = 10000
         self.x = x
         self.y = y 
         self.radius = radius
@@ -23,20 +21,14 @@ class Creature:
         # Genome
         self.weights = self.create_weights(inputs, hidden, output)
 
-        creature_count += 1
-
     def create_weights(self, inputs, hidden, output):
         mat_list = [np.random.rand(inputs, inputs) for _ in range(hidden)]
         mat_list.append(np.random.rand(output, inputs))
         return np.array(mat_list)
         
-    def draw(self, creature, screen):
-        x = creature.x
-        y = creature.y
-        radius = creature.radius
-        colour = creature.colour
-        pygame.gfxdraw.filled_circle(screen, x, y, radius, colour)
-        pygame.gfxdraw.aacircle(screen, x, y, radius, colour)
+    def draw(self, screen):
+        pygame.gfxdraw.filled_circle(screen, self.x, self.y, self.radius, self.colour)
+        pygame.gfxdraw.aacircle(screen, self.x, self.y, self.radius, self.colour)
 
     def get_count(self):
         return Creature.creature_count
