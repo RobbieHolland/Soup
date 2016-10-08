2c21c9092cb152c6365ae7e885d3cf892f980d61
Chris Hawkes <chawkesmexico@gmail.com>
2016-10-08 16:39:04 +0100

    Renamed Creature.py file

--- creature.py
+++ creature.py
@@ -3,16 +3,23 @@
 import pygame
 import pygame.gfxdraw
 import random as rd
+import numpy as np
 
 class Creature:
     creature_count = 0
 
-    def __init__(self, width, height, radius, colour):
+    def __init__(self, width, height, radius, colour, weights):
+
+        # Characteristics
         self.life = 0
         self.x = rd.randint(width-height,height-radius)
         self.y = rd.randint(width-height,height-radius)
         self.radius = radius
         self.colour = colour
+
+        # Genome
+        self.weights = np.
+
         creature_count += 1
         
     def draw(self, creature, screen):
