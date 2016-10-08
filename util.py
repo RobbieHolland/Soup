1a4b82f4859020dfb9362ff51aca9071a918cded
Chris Hawkes <chawkesmexico@gmail.com>
2016-10-08 16:23:59 +0100

    Creature class
    Basic functionality added to creature class

---
+++ util.py
@@ -0,0 +1,28 @@
+""" Creature class """
+
+import pygame
+import pygame.gfxdraw
+import random as rd
+
+class Creature:
+    creature_count = 0
+
+    def __init__(self, width, height, radius, colour):
+        self.life = 0
+        self.x = rd.randint(width-height,height-radius)
+        self.y = rd.randint(width-height,height-radius)
+        self.radius = radius
+        self.colour = colour
+        creature_count += 1
+        
+    def draw(self, creature, screen):
+        x = creature.x
+        y = creature.y
+        radius = creature.radius
+        colour = creature.colour
+        pygame.gfxdraw.filled_circle(screen, x, y, radius, colour)
+        pygame.gfxdraw.aacircle(screen, x, y, radius, colour)
+
+    def get_count(self):
+        return Creature.creature_count
+    
