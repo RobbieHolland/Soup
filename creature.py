1f2bc8a34dc544c1ad1f8d87b3ecc8fb315f9b4f
Chris Hawkes <chawkesmexico@gmail.com>
2016-10-08 17:42:26 +0100

    Added self param to create_weights`

--- creature.py
+++ creature.py
@@ -9,6 +9,9 @@ class Creature:
     creature_count = 0
 
     def __init__(self, x, y, radius, colour, inputs, hidden, output):
+        """
+        @param colour
+        """
 
         # Characteristics
         self.life = 0
@@ -22,7 +25,7 @@ class Creature:
 
         creature_count += 1
 
-    def create_weights(inputs, hidden, output):
+    def create_weights(self, inputs, hidden, output):
         mat_list = [np.random.rand(inputs, inputs) for _ in range(hidden)]
         mat_list.append(np.random.rand(output, inputs))
         return np.array(mat_list)
@@ -41,3 +44,5 @@ class Creature:
 
 if __name__ == '__main__':
     print("Test Compilation")
+    c = Creature(1,1,1,1,3,1,1)
+    print(c.weights)
